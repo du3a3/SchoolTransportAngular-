fd5ae67ea433bdf15cd25b2e9ca84501db7ba0ad
123usef <yousifm836@gmail>
2026-01-13 10:43:14 +0400

    error fixed

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.html',
   styleUrls: ['./layout.css']
 })
